6ce1af893c38ece0637ca11bbfe1f879962bc39c
Merritt Krakowitzer <mkrakowitzer@fnb.co.za>
2014-10-02 11:07:37 +0200

    Add externel facts for running verion of JIRA. This is in preperation to resolve issue #3
        The following facts are created:
         - jira_baseurl
         - jira_builddate
         - jira_buildnumber
         - jira_scminfo
         - jira_servertitle
         - jira_version
         - jira_versionnumbers

---
+++ spec/classes/jira_facts_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper.rb'
+describe 'jira' do
+  describe 'jira::facts' do
+    let(:params) {{
+      :javahome => '/opt/java',
+    }}
+    regexp_pe = /^#\!\/opt\/puppet\/bin\/ruby$/
+    regexp_oss = /^#\!\/usr\/bin\/env ruby$/
+    pe_external_fact_file = '/etc/puppetlabs/facter/facts.d/jira_facts.rb'
+    external_fact_file = '/etc/facter/facts.d/jira_facts.rb'
+
+    it { should contain_file(external_fact_file) }
+
+    # Test puppet enterprise shebang generated correctly
+    context 'with puppet enterprise' do
+        let(:facts) { {:puppetversion => "3.4.3 (Puppet Enterprise 3.2.1)"} }
+        it do
+          should contain_file(pe_external_fact_file) \
+            .with_content(regexp_pe)
+        end
+    end
+    # Test puppet oss shebang generated correctly
+    context 'with puppet oss' do
+        let(:facts) { {:puppetversion => "all other versions"} }
+        it do
+          should contain_file(external_fact_file) \
+            .with_content(regexp_oss)
+        end
+    end
+  end
+end
