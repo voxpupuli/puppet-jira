4992846060e857b4ef40124590c60fe22311925b
Krakowitzer, Merritt <merritt@krakowitzer.com>
2015-03-18 11:07:26 +0200

    Issue #63 Test that default URL exists.

--- spec/classes/jira_facts_spec.rb
+++ spec/classes/jira_facts_spec.rb
@@ -6,6 +6,7 @@ describe 'jira' do
     }}
     regexp_pe = /^#\!\/opt\/puppet\/bin\/ruby$/
     regexp_oss = /^#\!\/usr\/bin\/env ruby$/
+    regexp_url = /http\:\/\/127\.0\.0\.1\:8080\/rest\/api\/2\/serverInfo/
     pe_external_fact_file = '/etc/puppetlabs/facter/facts.d/jira_facts.rb'
     external_fact_file = '/etc/facter/facts.d/jira_facts.rb'
 
@@ -16,7 +17,8 @@ describe 'jira' do
         let(:facts) { {:puppetversion => "3.4.3 (Puppet Enterprise 3.2.1)"} }
         it do
           should contain_file(pe_external_fact_file) \
-            .with_content(regexp_pe)
+            .with_content(regexp_pe) \
+            .with_content(regexp_url)
         end
     end
     # Test puppet oss shebang generated correctly
@@ -24,7 +26,8 @@ describe 'jira' do
         let(:facts) { {:puppetversion => "all other versions"} }
         it do
           should contain_file(external_fact_file) \
-            .with_content(regexp_oss)
+            .with_content(regexp_oss) \
+            .with_content(regexp_url)
         end
     end
   end
