eb1a11c88db29107cd0b5e917b43c326450f3944
Merritt Krakowitzer <mkrakowiter@fnb.co.za>
2015-04-16 23:19:58 +0200

    fix syntax issue in tests

--- spec/classes/jira_config_spec.rb
+++ spec/classes/jira_config_spec.rb
@@ -114,59 +114,59 @@ describe 'jira::config' do
           }
         end
 
+        context 'mysql params' do
+          let(:params) {{
+            :version  => '6.3.4a',
+            :javahome => '/opt/java',
+            :db       => 'mysql',
+          }}
+          it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/bin/setenv.sh')}
+          it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/bin/user.sh')}
+          it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/conf/server.xml')}
+          it { should contain_file('/home/jira/dbconfig.xml')
+            .with_content(/<url>jdbc:mysql:\/\/localhost:5432\/jira\?useUnicode=true&amp;characterEncoding=UTF8&amp;sessionVariables=storage_engine=InnoDB<\/url>/) }
+        end
+        context 'sqlserver params' do
+          let(:params) {{
+            :version  => '6.3.4a',
+            :javahome => '/opt/java',
+            :db       => 'sqlserver',
+            :dbport   => '1433',
+          }}
+          it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/bin/setenv.sh')}
+          it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/bin/user.sh')}
+          it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/conf/server.xml')}
+          it { should contain_file('/home/jira/dbconfig.xml')
+            .with_content(/<url>jdbc:jtds:sqlserver:\/\/localhost:1433\/jira<\/url>/) }
+        end
+        context 'custom dburl' do
+          let(:params) {{
+            :version  => '6.3.4a',
+            :javahome => '/opt/java',
+            :dburl    => 'my custom dburl',
+          }}
+          it { should contain_file('/home/jira/dbconfig.xml')
+            .with_content(/<url>my custom dburl<\/url>/) }
+        end
+        context 'tomcat context path' do
+          let(:params) {{
+            :version => '6.3.4a',
+            :javahome => '/opt/java',
+            :contextpath => '/jira',
+          }}
+          it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/conf/server.xml')
+            .with_content(/<Context path=\"\/jira\" docBase=\"\${catalina.home}\/atlassian-jira\" reloadable=\"false\" useHttpOnly=\"true\">/) }
+        end
+        context 'context resources' do
+          let(:params) {{
+            :version => '6.3.4a',
+            :javahome => '/opt/java',
+            :resources => { 'testdb' => { 'auth' => 'Container' } },
+          }}
+          it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/conf/context.xml')
+            .with_content(/<Resource name = "testdb"\n        auth = "Container"\n    \/>/) }
+        end
       end
-    end
-    context 'mysql params' do
-      let(:params) {{
-        :version  => '6.3.4a',
-        :javahome => '/opt/java',
-        :db       => 'mysql',
-      }}
-      it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/bin/setenv.sh')}
-      it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/bin/user.sh')}
-      it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/conf/server.xml')}
-      it { should contain_file('/home/jira/dbconfig.xml')
-        .with_content(/<url>jdbc:mysql:\/\/localhost:5432\/jira\?useUnicode=true&amp;characterEncoding=UTF8&amp;sessionVariables=storage_engine=InnoDB<\/url>/) }
-    end
-    context 'sqlserver params' do
-      let(:params) {{
-        :version  => '6.3.4a',
-        :javahome => '/opt/java',
-        :db       => 'sqlserver',
-        :dbport   => '1433',
-      }}
-      it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/bin/setenv.sh')}
-      it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/bin/user.sh')}
-      it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/conf/server.xml')}
-      it { should contain_file('/home/jira/dbconfig.xml')
-        .with_content(/<url>jdbc:jtds:sqlserver:\/\/localhost:1433\/jira<\/url>/) }
-    end
-    context 'custom dburl' do
-      let(:params) {{
-        :version  => '6.3.4a',
-        :javahome => '/opt/java',
-        :dburl    => 'my custom dburl',
-      }}
-      it { should contain_file('/home/jira/dbconfig.xml')
-        .with_content(/<url>my custom dburl<\/url>/) }
-    end
-    context 'tomcat context path' do
-      let(:params) {{
-        :version => '6.3.4a',
-        :javahome => '/opt/java',
-        :contextpath => '/jira',
-      }}
-      it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/conf/server.xml')
-        .with_content(/<Context path=\"\/jira\" docBase=\"\${catalina.home}\/atlassian-jira\" reloadable=\"false\" useHttpOnly=\"true\">/) }
-    end
-    context 'context resources' do
-      let(:params) {{
-        :version => '6.3.4a',
-        :javahome => '/opt/java',
-        :resources => { 'testdb' => { 'auth' => 'Container' } },
-      }}
-      it { should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/conf/context.xml')
-        .with_content(/<Resource name = "testdb"\n        auth = "Container"\n    \/>/) }
     end
   end
 end
