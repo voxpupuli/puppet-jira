22f5df6681d3c63433374b92e3898734c4d5f6ae
Merritt Krakowitzer <mkrakowiter@fnb.co.za>
2015-04-16 13:36:24 +0200

    Refactor params.pp to use best practice

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,24 +6,29 @@ class jira::params {
 
   Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
 
-  case "${::osfamily}${::operatingsystemmajrelease}" {
-    /RedHat7/: {
-      $json_packages           = 'rubygem-json'
-      $service_file_location   = '/usr/lib/systemd/system/jira.service'
-      $service_file_template   = 'jira/jira.service.erb'
-      $service_lockfile        = '/var/lock/subsys/jira'
+  case $::osfamily {
+    /RedHat/: {
+      if $::operatingsystemmajrelease == '7' {
+        $json_packages           = 'rubygem-json'
+        $service_file_location   = '/usr/lib/systemd/system/jira.service'
+        $service_file_template   = 'jira/jira.service.erb'
+        $service_lockfile        = '/var/lock/subsys/jira'
+      } elsif $::operatingsystemmajrelease == '6' {
+        $json_packages           = [ 'rubygem-json', 'ruby-json' ]
+        $service_file_location   = '/etc/init.d/jira'
+        $service_file_template   = 'jira/jira.initscript.erb'
+        $service_lockfile        = '/var/lock/subsys/jira'
+      }
+    } /Debian/: {
+        $json_packages           = [ 'rubygem-json', 'ruby-json' ]
+        $service_file_location   = '/etc/init.d/jira'
+        $service_file_template   = 'jira/jira.initscript.erb'
+        $service_lockfile        = '/var/lock/jira'
+    } default: {
+        $json_packages           = [ 'rubygem-json', 'ruby-json' ]
+        $service_file_location   = '/etc/init.d/jira'
+        $service_file_template   = 'jira/jira.initscript.erb'
+        $service_lockfile        = '/var/lock/subsys/jira'
+      }
     }
-    /Debian/: {
-      $json_packages           = [ 'rubygem-json', 'ruby-json' ]
-      $service_file_location   = '/etc/init.d/jira'
-      $service_file_template   = 'jira/jira.initscript.erb'
-      $service_lockfile        = '/var/lock/jira'
-    }
-    default: {
-      $json_packages           = [ 'rubygem-json', 'ruby-json' ]
-      $service_file_location   = '/etc/init.d/jira'
-      $service_file_template   = 'jira/jira.initscript.erb'
-      $service_lockfile        = '/var/lock/subsys/jira'
-    }
-  }
 }
