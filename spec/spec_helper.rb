36e4bbfbb4d811bb618eb8a3b953c897b3533266
Adam Crews <Adam.Crews@gmail.com>
2015-10-07 12:19:36 -0700

    Setup the module for modulesync.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,15 +7,9 @@ RSpec.configure do |c|
     # avoid "Only root can execute commands as other users"
     Puppet.features.stubs(:root? => true)
   end
-end
-
-RSpec.configure do |c|
   c.default_facts = {
-    :jira_version    => '6.4',
+    :jira_version     => '6.4',
     :staging_http_get => 'curl',
-    #:os_maj_version   => '6',
-
-    :operatingsystemmajrelease => '6',
-    :puppetversion    => '3.7.4',
+    :puppetversion    => Puppet.version
   }
 end
