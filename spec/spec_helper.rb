0bd205669c7af663c134d51657c5eb234a95d5db
Merritt Krakowitzer <mkrakowitzer@fnb.co.za>
2015-10-13 19:36:57 +0200

    Update from puppet-community modulesync_configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,1 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'rspec-puppet-facts'
-include RspecPuppetFacts
-
-RSpec.configure do |c|
-  c.before do
-    # avoid "Only root can execute commands as other users"
-    Puppet.features.stubs(:root? => true)
-  end
-  c.default_facts = {
-    :jira_version     => '6.4',
-    :staging_http_get => 'curl',
-    :puppetversion    => Puppet.version
-  }
-end
