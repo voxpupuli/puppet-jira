8848264ccd288eec9d7a6f61da1fd16be6408fbb
Eugene <git@eugeny.me>
2015-10-16 15:28:08 +0500

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -13,20 +13,31 @@ class jira::params {
         $service_file_location   = '/usr/lib/systemd/system/jira.service'
         $service_file_template   = 'jira/jira.service.erb'
         $service_lockfile        = '/var/lock/subsys/jira'
+        $service_systemd         = true
       } elsif $::operatingsystemmajrelease == '6' or $::operatingsystem == 'Amazon'{
         $json_packages           = [ 'rubygem-json', 'ruby-json' ]
         $service_file_location   = '/etc/init.d/jira'
         $service_file_template   = 'jira/jira.initscript.erb'
         $service_lockfile        = '/var/lock/subsys/jira'
+        $service_systemd         = false
       } else {
         fail("\"${module_name}\" provides no service parameters
             for \"${::osfamily}\" - \"${::operatingsystemmajrelease}\"")
       }
     } /Debian/: {
+      if $::operatingsystemmajrelease == '8' {
+        $json_packages           = 'ruby-json'
+        $service_file_location   = '/lib/systemd/system/jira.service'
+        $service_file_template   = 'jira/jira.service.erb'
+        $service_lockfile        = '/var/lock/subsys/jira'
+        $service_systemd         = true
+      } else {
         $json_packages           = [ 'rubygem-json', 'ruby-json' ]
         $service_file_location   = '/etc/init.d/jira'
         $service_file_template   = 'jira/jira.initscript.erb'
         $service_lockfile        = '/var/lock/jira'
+        $service_systemd         = false
+      }
     } default: {
         $json_packages           = [ 'rubygem-json', 'ruby-json' ]
         $service_file_location   = '/etc/init.d/jira'
