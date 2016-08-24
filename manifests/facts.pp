85287165af692b2e1eb453109f6dd60e8f66025c
Richard Gareau <richard.gareau@bell.ca>
2016-08-24 16:36:05 -0400

    Detect if running in an AIO context and skip install of json_packages

--- manifests/facts.pp
+++ manifests/facts.pp
@@ -27,10 +27,14 @@ class jira::facts(
 
   # Puppet Enterprise supplies its own ruby version if your using it.
   # A modern ruby version is required to run the executable fact
+  # Since PE 2015.2 the agents are All in One (AIO) wich also include it's own ruby version
   if $::puppetversion =~ /Puppet Enterprise/ {
     $ruby_bin = '/opt/puppet/bin/ruby'
     $dir      = 'puppetlabs/'
-  } else {
+  } elsif $::aio_agent_version =~ /1\.3\.*[0-9]*/ {
+    $ruby_bin = '/opt/puppetlabs/puppet/bin/ruby'
+    $dir = 'puppetlabs/'
+   } else {
     $ruby_bin = '/usr/bin/env ruby'
     $dir      = ''
   }
@@ -46,7 +50,8 @@ class jira::facts(
     }
   }
 
-  if $::osfamily == 'RedHat' and $::puppetversion !~ /Puppet Enterprise/ {
+  # Install $json_packages only if osfamily is RedHat and agent is version PE 3.8 and less or not an AIO version 1.3.x
+  if $::osfamily == 'RedHat' and (($::puppetversion !~ /Puppet Enterprise/) and ($::aio_agent_version !~ /1\.3\.*[0-9]*/)) {
     package { $json_packages:
       ensure => present,
     }
@@ -59,3 +64,4 @@ class jira::facts(
   }
 
 }
+q
