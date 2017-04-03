1041f1c92de82e9e9e5d23ecadb14791dcb96a48
Tim Meusel <tim@bastelfreak.de>
2017-04-04 00:18:23 +0200

    replace validate_* with datatypes

--- manifests/sso.pp
+++ manifests/sso.pp
@@ -3,20 +3,16 @@
 # Install confluence SSO via crowd, See README.md for more.
 #
 class jira::sso(
-  $application_name = $::jira::application_name,
-  $application_password = $::jira::application_password,
-  $application_login_url = $::jira::application_login_url,
-  $crowd_server_url = $::jira::crowd_server_url,
-  $crowd_base_url = $::jira::crowd_base_url,
-  $session_isauthenticated = $::jira::session_isauthenticated,
-  $session_tokenkey = $::jira::session_tokenkey,
-  $session_validationinterval = $::jira::session_validationinterval,
-  $session_lastvalidation = $::jira::session_lastvalidation,
+  $application_name                                                 = $::jira::application_name,
+  $application_password                                             = $::jira::application_password,
+  Variant[Stdlib::HTTPUrl, Stdlib::HTTPSUrl] $application_login_url = $::jira::application_login_url,
+  Variant[Stdlib::HTTPUrl, Stdlib::HTTPSUrl] $crowd_server_url      = $::jira::crowd_server_url,
+  Variant[Stdlib::HTTPUrl, Stdlib::HTTPSUrl] $crowd_base_url        = $::jira::crowd_base_url,
+  $session_isauthenticated                                          = $::jira::session_isauthenticated,
+  $session_tokenkey                                                 = $::jira::session_tokenkey,
+  $session_validationinterval                                       = $::jira::session_validationinterval,
+  $session_lastvalidation                                           = $::jira::session_lastvalidation,
 ) {
-
-  validate_re($application_login_url,'^https?\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$')
-  validate_re($crowd_server_url,'^https?\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$')
-  validate_re($crowd_base_url,'^https?\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$')
 
   file { "${jira::webappdir}/atlassian-jira/WEB-INF/classes/crowd.properties":
     ensure  => present,
