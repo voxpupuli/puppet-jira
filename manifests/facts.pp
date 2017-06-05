edcafab1d93f72c82839835338efb849172c6766
Richard Gareau <richard.gareau@bell.ca>
2017-06-05 14:45:40 -0400

    Fix conflict

--- manifests/facts.pp
+++ manifests/facts.pp
@@ -14,7 +14,7 @@
 #
 # class { 'jira::facts': }
 #
-class jira::facts(
+class jira::facts (
   $ensure        = 'present',
   $port          = $jira::tomcatPort,
   $uri           = $jira::tomcatAddress ? {
@@ -22,39 +22,32 @@ class jira::facts(
     default => $jira::tomcatAddress,
   },
   $contextpath   = $jira::contextpath,
-  $json_packages = $jira::params::json_packages,
-) inherits jira::params {
-
+  $json_packages = $jira::params::json_packages,) inherits jira::params {
   # Puppet Enterprise supplies its own ruby version if your using it.
   # A modern ruby version is required to run the executable fact
   # Since PE 2015.2 the agents are All in One (AIO) wich also include it's own ruby version
   if $::puppetversion =~ /Puppet Enterprise/ {
     $ruby_bin = '/opt/puppet/bin/ruby'
-    $dir      = 'puppetlabs/'
+    $dir = 'puppetlabs/'
   } elsif $::aio_agent_version =~ /1\.*[0-9]*\.*[0-9]*/ {
     $ruby_bin = '/opt/puppetlabs/puppet/bin/ruby'
     $dir = 'puppetlabs/'
-   } else {
+  } else {
     $ruby_bin = '/usr/bin/env ruby'
-    $dir      = ''
+    $dir = ''
   }
 
-  if ! defined(File["/etc/${dir}facter"]) {
-    file { "/etc/${dir}facter":
-      ensure  => directory,
-    }
+  if !defined(File["/etc/${dir}facter"]) {
+    file { "/etc/${dir}facter": ensure => directory, }
   }
-  if ! defined(File["/etc/${dir}facter/facts.d"]) {
-    file { "/etc/${dir}facter/facts.d":
-      ensure  => directory,
-    }
+
+  if !defined(File["/etc/${dir}facter/facts.d"]) {
+    file { "/etc/${dir}facter/facts.d": ensure => directory, }
   }
 
   # Install $json_packages only if osfamily is RedHat and agent is version PE 3.8 and less or not an AIO version 1.3.x
   if $::osfamily == 'RedHat' and (($::puppetversion !~ /Puppet Enterprise/) and ($::aio_agent_version !~ /1\.*[0-9]*\.*[0-9]*/)) {
-    package { $json_packages:
-      ensure => present,
-    }
+    package { $json_packages: ensure => present, }
   }
 
   file { "/etc/${dir}facter/facts.d/jira_facts.rb":
@@ -64,4 +57,3 @@ class jira::facts(
   }
 
 }
-q
