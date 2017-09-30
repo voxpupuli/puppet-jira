d4b13a9c866ce7312948771ace5cdf53c2a53056
William Yardley <wby@axs.com>
2017-09-30 16:44:33 -0700

    Refactor acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,34 +1,32 @@
 require 'beaker-rspec'
-require 'beaker-rspec/spec_helper'
-require 'beaker-rspec/helpers/serverspec'
 require 'beaker/puppet_install_helper'
-install_puppet_agent_on hosts, {} unless ENV['BEAKER_provision'] == 'no'
+require 'beaker/module_install_helper'
+
+run_puppet_install_helper
+install_module
+install_module_dependencies
+
+# Install soft deps
+install_module_from_forge('puppetlabs-apt', '>= 4.1.0 < 5.0.0')
+install_module_from_forge('puppetlabs-java', '>= 2.1.0 < 3.0.0')
+install_module_from_forge('puppetlabs-java_ks', '>= 1.6.0 < 2.0.0')
+install_module_from_forge('puppetlabs-mysql', '>= 4.0.1 < 5.0.0')
+install_module_from_forge('puppetlabs-postgresql', '>= 5.1.0 < 6.0.0')
 
 UNSUPPORTED_PLATFORMS = %w[AIX windows Solaris].freeze
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
   # Readable test descriptions
   c.formatter = :documentation
 
-  # Configure all nodes in nodeset
   c.before :suite do
-    puppet_module_install(source: proj_root, module_name: 'jira')
     hosts.each do |host|
-      if fact('osfamily') == 'Debian'
-        on host, 'apt-get install -y locales'
-        on host, "echo \"en_US ISO-8859-1\nen_NG.UTF-8 UTF-8\nen_US.UTF-8 UTF-8\n\" > /etc/locale.gen"
-        on host, '/usr/sbin/locale-gen'
-        on host, '/usr/sbin/update-locale'
-      end
-      on host, puppet('module', 'install', 'puppet-archive'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'puppetlabs-java_ks'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'puppetlabs-mysql'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'puppetlabs-postgresql'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'puppet-staging'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), acceptable_exit_codes: [0, 1]
+      # Not sure if this should move into the spec itself; leaving here for now
+      next unless fact('osfamily') == 'Debian'
+      on host, 'apt-get install -y locales apt-transport-https'
+      on host, "echo \"en_US ISO-8859-1\nen_NG.UTF-8 UTF-8\nen_US.UTF-8 UTF-8\n\" > /etc/locale.gen"
+      on host, '/usr/sbin/locale-gen'
+      on host, '/usr/sbin/update-locale'
     end
   end
 end
