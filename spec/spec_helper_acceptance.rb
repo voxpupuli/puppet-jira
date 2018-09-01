e965600d75d669b89f880b794a4898d07875a3cb
Tim Meusel <tim@bastelfreak.de>
2018-09-01 13:59:21 +0200

    update spec_helper_acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,12 +1,23 @@
 require 'beaker-rspec'
+require 'beaker-puppet'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
-run_puppet_install_helper
-install_module
-install_module_dependencies
+run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
+install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
 
-# Install soft deps
+RSpec.configure do |c|
+  # Readable test descriptions
+  c.formatter = :documentation
+  hosts.each do |host|
+    if host[:platform] =~ %r{el-7-x86_64} && host[:hypervisor] =~ %r{docker}
+      on(host, "sed -i '/nodocs/d' /etc/yum.conf")
+    end
+  end
+end
+
 install_module_from_forge('puppetlabs-apt', '>= 4.1.0 < 5.0.0')
 install_module_from_forge('puppetlabs-java', '>= 2.1.0 < 3.0.0')
 install_module_from_forge('puppetlabs-java_ks', '>= 1.6.0 < 2.0.0')
