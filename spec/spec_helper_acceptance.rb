c0df825a016f7661052a9c445d572ab04c37d027
Tim Meusel <tim@bastelfreak.de>
2018-09-01 14:00:30 +0200

    allow puppetlabs/java 3.x

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -19,7 +19,7 @@ RSpec.configure do |c|
 end
 
 install_module_from_forge('puppetlabs-apt', '>= 4.1.0 < 7.0.0')
-install_module_from_forge('puppetlabs-java', '>= 2.1.0 < 3.0.0')
+install_module_from_forge('puppetlabs-java', '>= 2.1.0 < 4.0.0')
 install_module_from_forge('puppetlabs-java_ks', '>= 1.6.0 < 2.0.0')
 install_module_from_forge('puppetlabs-mysql', '>= 4.0.1 < 6.0.0')
 install_module_from_forge('puppetlabs-postgresql', '>= 5.1.0 < 6.0.0')
