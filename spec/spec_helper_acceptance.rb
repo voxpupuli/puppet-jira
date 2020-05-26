e052f8df956314a315b06bf64b62a09d21e34f7e
Dennis Hoppe <github@debian-solutions.de>
2020-05-26 16:20:02 +0200

    Fix module dependencies for acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,11 +1,12 @@
+# This file is completely managed via modulesync
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-configure_beaker do |host|
-  if fact_on(host, 'os.name') == 'CentOS'
-    install_module_from_forge('puppetlabs-apt', '>= 4.1.0 < 8.0.0')
-    install_module_from_forge('puppetlabs-java', '>= 2.1.0 < 7.0.0')
-    install_module_from_forge('puppetlabs-java_ks', '>= 1.6.0 < 4.0.0')
-    install_module_from_forge('puppetlabs-mysql', '>= 4.0.1 < 11.0.0')
-    install_module_from_forge('puppetlabs-postgresql', '>= 5.1.0 < 7.0.0')
-  end
+configure_beaker do |_host|
+  install_module_from_forge('puppetlabs-apt', '>= 4.1.0 < 8.0.0') if fact('os.family') == 'Debian'
+  install_module_from_forge('puppetlabs-java', '>= 2.1.0 < 7.0.0')
+  install_module_from_forge('puppetlabs-java_ks', '>= 1.6.0 < 4.0.0')
+  install_module_from_forge('puppetlabs-mysql', '>= 4.0.1 < 11.0.0')
+  install_module_from_forge('puppetlabs-postgresql', '>= 5.1.0 < 7.0.0')
 end
+
+Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
