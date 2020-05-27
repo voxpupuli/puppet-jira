81845d60a697e205363af612cf86d2271b6aae73
Dennis Hoppe <github@debian-solutions.de>
2020-05-27 19:47:03 +0200

    Use node specific syntax
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,12 +1,12 @@
 # This file is completely managed via modulesync
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-configure_beaker do |_host|
-  install_module_from_forge('puppetlabs-apt', '>= 4.1.0 < 8.0.0') if fact('os.family') == 'Debian'
-  install_module_from_forge('puppetlabs-java', '>= 2.1.0 < 7.0.0')
-  install_module_from_forge('puppetlabs-java_ks', '>= 1.6.0 < 4.0.0')
-  install_module_from_forge('puppetlabs-mysql', '>= 4.0.1 < 11.0.0')
-  install_module_from_forge('puppetlabs-postgresql', '>= 5.1.0 < 7.0.0')
+configure_beaker do |host|
+  install_module_from_forge_on(host, 'puppetlabs-apt', '>= 4.1.0 < 8.0.0') if fact_on(host, 'os.family') == 'Debian'
+  install_module_from_forge_on(host, 'puppetlabs-java', '>= 2.1.0 < 7.0.0')
+  install_module_from_forge_on(host, 'puppetlabs-java_ks', '>= 1.6.0 < 4.0.0')
+  install_module_from_forge_on(host, 'puppetlabs-mysql', '>= 4.0.1 < 11.0.0')
+  install_module_from_forge_on(host, 'puppetlabs-postgresql', '>= 5.1.0 < 7.0.0')
 end
 
 Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
