cb8727aa0fb9da19113c1da74bad7532aeb6cb9a
Lucas Emery <lemery@bottomline.com>
2020-08-26 14:51:53 -0400

    Adding connection properties as a hash

--- manifests/init.pp
+++ manifests/init.pp
@@ -76,6 +76,7 @@ class jira (
   $pool_min_size                                                    = 20,
   $pool_max_size                                                    = 20,
   $pool_max_wait                                                    = 30000,
+  Hash $connection_properties                                       = {},
   $validation_query                                                 = undef,
   $min_evictable_idle_time                                          = 60000,
   $time_between_eviction_runs                                       = undef,
