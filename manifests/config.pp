7ac3ee72150d9fd90d66624b84529b4057aa1aa0
Jarkko Oranen <oranenj@iki.fi>
2021-04-14 15:08:11 +0300

    Clean up config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -13,7 +13,12 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 # -----------------------------------------------------------------------------
-class jira::config inherits jira {
+class jira::config {
+
+  # This class should be used from init.pp with a dependency on jira::install
+  # and sending a refresh to jira::service
+  assert_private()
+
   File {
     owner => $jira::user,
     group => $jira::group,
@@ -30,65 +35,45 @@ class jira::config inherits jira {
   file { "${jira::webappdir}/bin/user.sh":
     content => template('jira/user.sh.erb'),
     mode    => '0755',
-    require => [
-      Class['jira::install'],
-      File[$jira::webappdir],
-      File[$jira::homedir],
-    ],
   }
 
-  -> file { "${jira::webappdir}/bin/setenv.sh":
+  file { "${jira::webappdir}/bin/setenv.sh":
     content => template('jira/setenv.sh.erb'),
     mode    => '0755',
-    require => Class['jira::install'],
-    notify  => Class['jira::service'],
   }
 
-  -> file { "${jira::homedir}/dbconfig.xml":
+  file { "${jira::homedir}/dbconfig.xml":
     content => epp('jira/dbconfig.xml.epp'),
     mode    => '0600',
-    require => [Class['jira::install'],File[$jira::homedir]],
-    notify  => Class['jira::service'],
   }
 
   if $jira::script_check_java_manage {
     file { "${jira::webappdir}/bin/check-java.sh":
       content => template($jira::script_check_java_template),
       mode    => '0755',
-      require => [
-        Class['jira::install'],
-        File["${jira::webappdir}/bin/setenv.sh"],
-      ],
-      notify  => Class['jira::service'],
+      require => File["${jira::webappdir}/bin/setenv.sh"],
     }
   }
 
   file { "${jira::webappdir}/conf/server.xml":
     content => template('jira/server.xml.erb'),
     mode    => '0600',
-    require => Class['jira::install'],
-    notify  => Class['jira::service'],
   }
-  -> file { "${jira::webappdir}/conf/context.xml":
+
+  file { "${jira::webappdir}/conf/context.xml":
     content => template('jira/context.xml.erb'),
     mode    => '0600',
-    require => Class['jira::install'],
-    notify  => Class['jira::service'],
   }
 
   file { "${jira::homedir}/jira-config.properties":
     content => template('jira/jira-config.properties.erb'),
     mode    => '0600',
-    require => [Class['jira::install'],File[$jira::homedir]],
-    notify  => Class['jira::service'],
   }
 
   if $jira::datacenter {
     file { "${jira::homedir}/cluster.properties":
       content => template('jira/cluster.properties.erb'),
       mode    => '0600',
-      require => [Class['jira::install'],File[$jira::homedir]],
-      notify  => Class['jira::service'],
     }
   }
 }
