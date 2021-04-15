54f6ae547720a2761b2d7916dd1e3dda7edf3fdf
Jarkko Oranen <oranenj@iki.fi>
2021-04-15 18:09:04 +0300

    Properly document jira::sorted_hash as private
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- functions/sort_hash.pp
+++ functions/sort_hash.pp
@@ -1,4 +1,6 @@
-function jira::sort_hash(Hash $input) {
+# @summary Return a sorted hash
+# @api private
+function jira::sort_hash(Hash $input) >> Hash {
   # Puppet hashes are "insertion order", so this works to sort by key
   Hash(sort(Array($input)))
 }
