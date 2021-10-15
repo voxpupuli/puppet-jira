0d9d98e69fa9dd6cfd1ff6c1e3bfa05181ec088b
Romain Tartière <romain@blogreen.org>
2021-10-14 15:30:45 -1000

    Allow mysql 12.x

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -5,7 +5,7 @@ configure_beaker do |host|
   install_module_from_forge_on(host, 'puppetlabs-apt', '>= 4.1.0 < 9.0.0') if fact_on(host, 'os.family') == 'Debian'
   install_module_from_forge_on(host, 'puppetlabs-java', '>= 2.1.0 < 8.0.0')
   install_module_from_forge_on(host, 'puppetlabs-java_ks', '>= 1.6.0 < 4.0.0')
-  install_module_from_forge_on(host, 'puppetlabs-mysql', '>= 4.0.1 < 12.0.0')
+  install_module_from_forge_on(host, 'puppetlabs-mysql', '>= 4.0.1 < 13.0.0')
   install_module_from_forge_on(host, 'puppetlabs-postgresql', '>= 5.1.0 < 8.0.0')
 end
 
