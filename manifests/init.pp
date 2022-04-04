36b9d3a7acf85db4f04c545c53c7ee6ffa9ac848
Christos Papageorgiou <christos.papageorgioy@gmail.com>
2022-04-04 18:31:49 +0300

    Remove unused parameter catalina_opts

--- manifests/init.pp
+++ manifests/init.pp
@@ -144,8 +144,6 @@
 #   Configures JVM_EXTRA_ARGS in setenv.sh
 # @param jvm_nofiles_limit
 #   Set the limit for open files
-# @param catalina_opts
-#   Does nothing :-)
 # @param download_url
 #   Base URL for downloading Atlassian software
 # @param checksum
@@ -353,7 +351,6 @@ class jira (
   Optional[String] $jvm_code_cache_args                             = undef,
   Optional[String] $jvm_extra_args                                  = undef,
   Integer $jvm_nofiles_limit                                        = 16384,
-  String $catalina_opts                                             = '',
   # Misc Settings
   Stdlib::HTTPUrl $download_url                                     = 'https://product-downloads.atlassian.com/software/jira/downloads',
   Optional[String] $checksum                                        = undef,
