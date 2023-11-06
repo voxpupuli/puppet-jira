43bb4d1866587fbf5e54e0fc18211976831652c0
Daniel Fernández <daferoes@gmail.com>
2023-11-06 15:43:56 +0100

    Replace install_module_from_forge_on by install_puppet_module_via_pmt_on

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,11 +4,11 @@
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
-  install_module_from_forge_on(host, 'puppetlabs-apt', '>= 4.1.0 < 9.0.0') if fact_on(host, 'os.family') == 'Debian'
-  install_module_from_forge_on(host, 'puppetlabs-java', '>= 2.1.0 < 8.0.0')
-  install_module_from_forge_on(host, 'puppetlabs-java_ks', '>= 1.6.0 < 4.0.0')
-  install_module_from_forge_on(host, 'puppetlabs-mysql', '>= 4.0.1 < 13.0.0')
-  install_module_from_forge_on(host, 'puppetlabs-postgresql', '>= 5.1.0 < 8.0.0')
+  install_puppet_module_via_pmt_on(host, 'puppetlabs-apt') if fact_on(host, 'os.family') == 'Debian'
+  install_puppet_module_via_pmt_on(host, 'puppetlabs-java')
+  install_puppet_module_via_pmt_on(host, 'puppetlabs-java_ks')
+  install_puppet_module_via_pmt_on(host, 'puppetlabs-mysql')
+  install_puppet_module_via_pmt_on(host, 'puppetlabs-postgresql')
 end
 
 Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
