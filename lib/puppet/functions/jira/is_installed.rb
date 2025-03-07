f81a968144326bb8df0f75df2f25e9bcd123ae81
diLLec <michael@kluge.ws>
2025-03-07 08:46:27 +0100

    remove facts in favor of a Deferred function (fix lint)

--- lib/puppet/functions/jira/is_installed.rb
+++ lib/puppet/functions/jira/is_installed.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
-#
+
 Puppet::Functions.create_function(:'jira::is_installed') do
   dispatch :default_impl do
     param 'String[1]', :jira_user
     return_type 'Boolean'
   end
   def default_impl(jira_user)
-    begin
-      File.exist? format('%s/dbconfig.xml', Dir.home(jira_user))
-    rescue
-      false
-    end
+    File.exist? format('%s/dbconfig.xml', Dir.home(jira_user))
+  rescue StandardError
+    false
   end
 end
