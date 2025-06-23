e13a1ce76e65ccdb5cf69be7ffacf34a21da864c
Kenyon Ralph <kralph@qti.qualcomm.com>
2025-06-23 15:19:48 -0700

    Drop support for EOL Ubuntu 18.04 and Ubuntu 20.04
    Since we disable AppArmor with
    https://github.com/voxpupuli/gha-puppet/pull/72 we should be able to
    remove the AppArmor workaround, which this commit does.

--- spec/acceptance/mysql_spec.rb
+++ spec/acceptance/mysql_spec.rb
@@ -5,28 +5,10 @@ require 'spec_helper_acceptance'
 describe 'jira mysql' do
   it 'installs with defaults' do
     pp = <<-EOS
-      # On ubuntu 20.04 and 22.04 the default is to install mariadb
-      # As the ubuntu 20.04 runner we use in github actions allready has mysql installed
-      # a apparmor error is triggerd when using mariadb in this test..
-      # Forcing the use of mysql
-      if $facts['os']['name'] == 'Ubuntu' and versioncmp($facts['os']['release']['major'], '20.04') >= 0 {
-        $mysql_service_name = 'mysql'
-        $mysql_server_package = 'mysql-server'
-        $mysql_client_package = 'mysql-client'
-      } else {
-        $mysql_service_name = undef
-        $mysql_server_package = undef
-        $mysql_client_package = undef
-      }
-
       class { 'mysql::server':
         root_password => 'strongpassword',
-        package_name => $mysql_server_package,
-        service_name => $mysql_service_name,
       }
-      class { 'mysql::client':
-        package_name => $mysql_client_package,
-      }
+      include mysql::client
 
       $cs = 'utf8mb4'
 
